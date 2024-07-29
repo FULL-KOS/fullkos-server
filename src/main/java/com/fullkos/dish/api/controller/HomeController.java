dde01e31f1004266372c7b2ec53c15799ca8def9
bongmin kang <kbm4250@naver.com>
2024-07-29 20:31:13 +0900

    feat | Impl test controller

---
+++ src/main/java/com/fullkos/dish/api/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.fullkos.dish.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<? extends BaseResponseBody> home() {
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(HttpStatus.OK, "test"));
+    }
+}
