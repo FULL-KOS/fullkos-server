d705678d12644e7b28dc443a03ef6a4e7b0e04e0
bongmin kang <kbm4250@naver.com>
2024-08-03 18:08:30 +0900

    fix | remove unuse import

--- src/main/java/com/fullkos/dish/db/repository/CompanyRepository.java
+++ src/main/java/com/fullkos/dish/db/repository/CompanyRepository.java
@@ -4,8 +4,6 @@ import com.fullkos.dish.db.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
--- src/main/java/com/fullkos/dish/db/repository/PortfolioRepository.java
+++ src/main/java/com/fullkos/dish/db/repository/PortfolioRepository.java
@@ -4,8 +4,6 @@ import com.fullkos.dish.db.entity.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 }
